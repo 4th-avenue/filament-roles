dcce3d25ad600aa52a86ff16fd0dffa6f82b270f
4th-avenue <quebec706@gmail.com>
2024-09-13 16:25:20 +0900

    filament 관리자 메뉴의 아이콘과 순서 변경하고 그룹화

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -19,7 +19,9 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+    protected static ?int $navigationSort = 1;
+    protected static ?string $navigationGroup = 'Settings';
 
     public static function form(Form $form): Form
     {
