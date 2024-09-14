aa8483c3b82e051730eedb969eecd7af057aa0ea
4th-avenue <quebec706@gmail.com>
2024-09-14 17:04:06 +0900

    PostPolicy를 이용하여 Post 모델에 대한 권한 정책 설정

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -58,6 +58,7 @@ class PostResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
